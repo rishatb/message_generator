68d213ff66e9ecd3bef2638785de3542adef6c24
freemailik <bulatov-rishat@mail.ru>
2022-08-21 17:06:14 +0500

    added object and array of words

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-signs = ['aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 'scorpio', 'sagittarius', 'capricom', 'aquarius', 'pisces']
+Arrays = {
+    signs: ['aries', 'taurus', 'gemini', 'cancer', 'leo', 'virgo', 'libra', 'scorpio', 'sagittarius', 'capricom', 'aquarius', 'pisces'],
+
+    words: ['make your homework', 'play videogames', 'stay at home', 'leave your house', 'go to the gym', 'eat some grass', 'draw something cute']
+}
 
 const randSign = (arr) => {
-    randIndex = Math.floor(Math.random() * 15);
-    sign = arr[randIndex];
+    randIndex = Math.floor(Math.random() * 12);
+    sign = arr.signs[randIndex];
     return sign;
 }
 
 
-console.log(randSign(signs));
+console.log(randSign(Arrays));
 
 // control return
